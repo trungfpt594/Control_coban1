883fbe552b5289b19ea13f2b89a1387ab7298409
trungfpt594 <nguyenchitrung800@gmail.com>
2025-06-15 14:36:49 +0700

    Add files via upload

---
+++ ButtonControl_TongHop/ButtonControl_TongHop/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace ButtonControl_TongHop
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnThoat_Click(object sender, RoutedEventArgs e)
+        {
+            Close(); // thoát
+        }
+
+        private void BtnTong_Click(object sender, RoutedEventArgs e)
+        {
+            int a = int.Parse(txtA.Text);
+            int b = int.Parse(txtB.Text);
+            int tong = a + b;
+            tbKetQua.Text = tong + "";
+        }
+    }
+}
